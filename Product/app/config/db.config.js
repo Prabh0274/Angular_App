640a7fb864801c0df156a4970418ce10850f7b60
Prabh0274 <prabh28868@gmail.com>
2023-07-07 12:29:09 -0400

    first commit

---
+++ Product/app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+  //url: "mongodb://localhost:27017/Blessing_db"
+
+ 
+
+  //ATLAS_URI=mongodb + srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/tutorials?retryWrites=true&w=majority;
+
+   //url:"mongodb+srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/Tutorial?retryWrites=true&w=majority"
+
+url:"mongodb+srv://prabh28868:Prabh8847256693@cluster0.r7fo8qu.mongodb.net/DressStore?retryWrites=true&w=majority"
+
+};
